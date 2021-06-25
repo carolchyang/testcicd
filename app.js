20707683eb2a162b3f6e2fb0157acb32a646c529
carolchyang <chingheui.yang@gmail.com>
2021-06-26 05:01:30 +0800

    test3

--- app.js
+++ app.js
@@ -6,49 +6,60 @@ const googleUrl = 'https://script.google.com/macros/s/AKfycbyFRu0xZkYKd5NhLp5dQI
 const url = 'https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=tR9TIFWlvquB';
 
 function getData() {
-  const data = [
-    {
-      name: 'a',
-      id: 1
-    },
-    {
-      name: 'b',
-      id: 2
-    },
-    {
-      name: 'c',
-      id: 3
-    },
-  ];
+  // const data = [
+  //   {
+  //     name: 'a',
+  //     id: 1
+  //   },
+  //   {
+  //     name: 'b',
+  //     id: 2
+  //   },
+  //   {
+  //     name: 'c',
+  //     id: 3
+  //   },
+  // ];
 
-  const time = Date.now();
+  // const time = Date.now();
 
-  data.push({
-    name: time,
-    id: time
-  })
+  // data.push({
+  //   name: time,
+  //   id: time
+  // })
 
-  fs.writeFile(path.join(__dirname, 'data.json'), JSON.stringify(data), (err) => {
-    if (err)
-      console.log(err);
-    else
-      console.log('更新完成');
-  });
+  // fs.writeFile(path.join(__dirname, 'data.json'), JSON.stringify(data), (err) => {
+  //   if (err)
+  //     console.log(err);
+  //   else
+  //     console.log('更新完成');
+  // });
   
-  // axios.get(`${googleUrl}?url=${url}`)
-  //   .then((response) => {
-  //     const data = JSON.stringify(response.data);
+  axios.get(`${googleUrl}?url=${url}`)
+    .then((response) => {
+      const data = [];
+      data.push(response.data[0]);
 
-  //     fs.writeFile(path.join(__dirname, 'data.json'), data, (err) => {
-  //       if (err)
-  //         console.log(err);
-  //       else
-  //         console.log('更新完成');
-  //     });
-  //   })
-  //   .catch((error) => {
-  //     console.log(error);
-  //   })
+      const time = Date.now();
+
+      data.push({
+        name: time,
+        id: time
+      })
+
+      // console.log(response.data[0]);
+      // const data = JSON.stringify(response.data);
+
+      fs.writeFile(path.join(__dirname, 'data.json'), JSON.stringify(data), (err) => {
+        if (err)
+          console.log(err);
+        else
+          console.log('更新完成');
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    })
 }
 
 getData();
