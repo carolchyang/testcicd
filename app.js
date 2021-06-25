42cb43be1e4603fba6ff3acc1837980b0224bbdc
carolchyang <chingheui.yang@gmail.com>
2021-06-26 04:48:48 +0800

    test

--- app.js
+++ app.js
@@ -6,20 +6,49 @@ const googleUrl = 'https://script.google.com/macros/s/AKfycbyFRu0xZkYKd5NhLp5dQI
 const url = 'https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=tR9TIFWlvquB';
 
 function getData() {
-  axios.get(`${googleUrl}?url=${url}`)
-    .then((response) => {
-      const data = JSON.stringify(response.data);
+  const data = [
+    {
+      name: 'a',
+      id: 1
+    },
+    {
+      name: 'b',
+      id: 2
+    },
+    {
+      name: 'c',
+      id: 3
+    },
+  ];
 
-      fs.writeFile(path.join(__dirname, 'data.json'), data, (err) => {
-        if (err)
-          console.log(err);
-        else
-          console.log('更新完成');
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    })
+  const time = Date.now();
+
+  data.push({
+    name: time,
+    id: time
+  })
+
+  fs.writeFile(path.join(__dirname, 'data.json'), data, (err) => {
+    if (err)
+      console.log(err);
+    else
+      console.log('更新完成');
+  });
+  
+  // axios.get(`${googleUrl}?url=${url}`)
+  //   .then((response) => {
+  //     const data = JSON.stringify(response.data);
+
+  //     fs.writeFile(path.join(__dirname, 'data.json'), data, (err) => {
+  //       if (err)
+  //         console.log(err);
+  //       else
+  //         console.log('更新完成');
+  //     });
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   })
 }
 
 getData();
